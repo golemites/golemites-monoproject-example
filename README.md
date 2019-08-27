de288e3f31f33cfc673cd1c2d2acac6925b94b3c
Toni Menzel <toni.menzel@rebaze.com>
2019-08-27 12:41:33 +0200

    init

---
+++ README.md
@@ -0,0 +1,8 @@
+# Golemites Demo Application
+
+This is a demo application for Golemites.
+
+This demo is structed as a single project setup with no (build time) subprojects.
+
+For more info, head over to https://github.com/golemites/golemites
+
---
+++ src/main/java/org/golemites/example/service/RESTEndpoint.java
@@ -0,0 +1,30 @@
+package org.golemites.example.calculator.internal;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.ServiceScope;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import static org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants.JAX_RS_APPLICATION_SELECT;
+import static org.osgi.service.jaxrs.whiteboard.JaxrsWhiteboardConstants.JAX_RS_RESOURCE;
+
+@Component(
+        property = {
+                JAX_RS_APPLICATION_SELECT + "=(osgi.jaxrs.name=.default)",
+                JAX_RS_RESOURCE + "=true",
+        },
+        scope = ServiceScope.PROTOTYPE,
+        service = RESTEndpoint.class
+)
+@Path( "/" )
+public class RESTEndpoint {
+
+    @GET
+    @Path("/foo")
+    @Produces( MediaType.APPLICATION_JSON )
+    public String sayHello() {
+        return "{\"message\" : \"Mono1\"}";
+    }
+}
